681d4620bbcb122e208229207b35c73f01ef715b
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-05-21 19:16:16 +0530

    routing improved, code cleanup

--- app/app.js
+++ app/app.js
@@ -1,23 +1,19 @@
-angular.module('codeSnip', ['ngRoute', 'AdalAngular'])
-.config(['$routeProvider','$httpProvider', 'adalAuthenticationServiceProvider'], 
+var codeSnip = angular.module('codeSnip', ['ngRoute','AdalAngular']);
+codeSnip.config(['$routeProvider','$httpProvider', 'adalAuthenticationServiceProvider',
 function($routeProvider, $httpProvider, adalProvider){
     $routeProvider
     .when("/", {
-        templateUrl: 'index.html',
-        requireADLogin:true
-    })
-    .when("/test", {
-        templateUrl: 'test.html',
+        templateUrl: 'user/home.html',
         requireADLogin:true
     })
     .when("/admin",{
         templateUrl:'admin/addSnip.html',
         requireADLogin:true
     });
-});
 
-adalProvider.init({
-    instance:'https://login.microsoftonline.com',
-    tenant:'https://azureadapp.azurewebsites.com',
-    clientId:'9c607d49-08e1-4ac4-8f57-b2ed6d119a74'
-}, $httpProvider);
+    adalProvider.init({
+        instance:'https://login.microsoftonline.com/',
+        tenant: 'bnarasimha21outlook.onmicrosoft.com',
+        clientId:'635c77c2-cdbd-4623-84db-896707e5ae6f'
+    }, $httpProvider);
+}]);
