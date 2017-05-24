97abcab7f9730b5b8b8fb113bb4836b0d5ef9d87
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-05-24 20:38:04 +0530

    changed look of website

--- app/admin/snipDetailController.js
+++ app/admin/snipDetailController.js
@@ -3,7 +3,9 @@
 
     function snipDetailController($scope, $routeParams){
         $scope.ID = $routeParams.ID;
-    }
+
+        $scope.snipDetail = {'id':'1','language':'c#', 'title':'How to add two dates', 'CodeSnippet':"var localDateTime = new LocalDate(2000, 1, 10).AtMidnight();var period = new PeriodBuilder {Years = 11, Months = 2, Days = 5,Hours = 10, Minutes = 10, Seconds = 11}.Build();var result = localDateTime + period;"}
+    }    
 
     angular.module('codeSnip').controller('snipDetailController', snipDetailController);
 })();
