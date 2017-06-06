3aaf88ce459b998e27c5703e329de4a1407d3c4c
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-06-06 21:53:12 +0530

    remove commented code

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 var gulp = require('gulp');
-var zip = require("gulp-zip");
-
-// gulp.task('deploy', ['build'], function(){
-//     return gulp.src(['**'])
-//     .pipe(zip('deploy.zip'))
-//     .pipe(gulp.dest('../'))
-// });
+var zip = require('gulp-zip');
 
 gulp.task('deploy', function(){
     return gulp.src(['**'])
