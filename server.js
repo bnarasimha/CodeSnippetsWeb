1cb66f02e0f103afe279eccbeee0d8abc2286eeb
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-06-19 13:00:23 +0530

    changed port to variable and APiUrl to Ubuntu Api server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var port = 80;
 
 app.use(express.static(__dirname + '/app'));
-app.listen(8080);
-console.log('App is listening in port 8080')
+app.listen(port);
+console.log('App is listening in port %d', port)
