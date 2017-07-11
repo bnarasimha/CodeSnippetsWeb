394d5a0a630ea4852c7b68bea737f79d3ea4a45d
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-11 14:57:23 +0530

    removed chmod 777 config in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ cd main;
 sudo rm -r node_modules; 
 unzip -o ~/artifacts/main/deploy.zip; 
 sudo chmod 777 node_modules; 
-sudo chmod 777 config;
 npm install; 
 pm2 delete main;
 pm2 start server.js --name main;
