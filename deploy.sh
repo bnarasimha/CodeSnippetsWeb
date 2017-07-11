21e9f877a346e643d3bb26f42626c782fa9073eb
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-11 15:05:00 +0530

    made unzip sudo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ set -x;
 export NODE_ENV=$1; 
 cd main; 
 sudo rm -r node_modules; 
-unzip -o ~/artifacts/main/deploy.zip; 
+sudo unzip -o ~/artifacts/main/deploy.zip; 
 sudo chmod 777 node_modules; 
 npm install; 
 pm2 delete main;
