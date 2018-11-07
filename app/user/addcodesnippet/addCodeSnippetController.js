27795aa0cb42cd97f9677c9c5c72a836f8a6e145
Narasimha Badrinath <bnarasimha21@gmail.com>
2018-11-07 22:35:27 +0530

    some more changes

--- app/user/addcodesnippet/addCodeSnippetController.js
+++ app/user/addcodesnippet/addCodeSnippetController.js
@@ -2,40 +2,36 @@
 
     'use strict'
 
-    function addCodeSnippetController($rootScope, $scope, $http, $location, addCodeSnippetService, languageService) {
+    function addCodeSnippetController($rootScope, $scope, $http, $location, addCodeSnippetService) {
         
             var hostUrl = 'http://' + $location.host() + ':' + $location.port() + '/#/';
-
-            languageService.getlanguages().then(function(response){
-                $scope.languages = response.data;
-            });
 
             $scope.Back = function(){
                 location.href = hostUrl;
             }
 
-            $scope.Submit = function () {
-            var codeSnippet = {
-                language: $scope.language,
-                title: $scope.title,
-                codesnippet: $scope.codeSnip,
-                userId: $rootScope.userId
+            $scope.addCodeSnippet = function () {
+                var codeSnippet = {
+                    language: $scope.language,
+                    title: $scope.title,
+                    codesnippet: $scope.codeSnip,
+                    userId: $rootScope.userId
+                };
+
+                addCodeSnippetService.addCodeSnippet(codeSnippet).then(function () {
+                    console.log('Added successfully');
+                })
+
+                alert('Added Successfully');
+                $scope.ClearFields();
             };
 
-            addCodeSnippetService.addCodeSnippet(codeSnippet).then(function (response) {
-                console.log('Added successfully');
-            })
-
-            alert('Added Successfully');
-            $scope.ClearFields();
-        };
-
-        $scope.ClearFields = function () {
-            $scope.language = '';
-            $scope.title = '';
-            $scope.codeSnip = '';
-        };
+            $scope.ClearFields = function () {
+                $scope.language = '';
+                $scope.title = '';
+                $scope.codeSnip = '';
+            };
     };
     
-    angular.module('codeSnip').controller('addCodeSnippetController', ['$rootScope','$scope', '$http', '$location', 'addCodeSnippetService', 'languageService', addCodeSnippetController])
+    angular.module('codeSnip').controller('addCodeSnippetController', ['$rootScope','$scope', '$http', '$location', 'addCodeSnippetService', addCodeSnippetController])
 })();
