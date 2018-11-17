2feaef95e5b54892a6095c6f264569ee7f722aeb
Narasimha Badrinath <bnarasimha21@gmail.com>
2018-11-18 01:19:22 +0530

    variable sub

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 set -x;
 export NODE_ENV=$1; 
-cd prod/main; 
+cd $1/main; 
 sudo rm -r node_modules; 
-sudo unzip -o ~/artifacts/prod/main/deploy.zip; 
+sudo unzip -o ~/artifacts/$1/main/deploy.zip; 
 sudo chmod 777 node_modules; 
 sudo chmod -R 777 app;
 sudo npm install; 
 gulp config;
-pm2 delete prodmain;
-pm2 start server.js --name prodmain;
+pm2 delete $1main;
+pm2 start server.js --name $1main;
