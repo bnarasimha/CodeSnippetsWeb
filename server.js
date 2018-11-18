218d2f1c496ec635cdd92efe6ffb61c2c26fabaa
Narasimha Badrinath <bnarasimha21@gmail.com>
2018-11-18 10:40:19 +0530

    different ports for dev and test

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+
 var port = 8080;
+if(process.env.NODE_ENV == 'test')
+port = 8081;
 
 app.use(express.static(__dirname + '/app'));
 app.listen(port);
