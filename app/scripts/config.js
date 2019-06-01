811b817d58e005c09147939e93fcde31b2682084
Narasimha Badrinath <bnarasimha21@gmail.com>
2019-06-01 18:36:19 +0530

    Testing with old api url

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
-// angular.module("codeSnip")
-// .constant("ApiUrl", "http://ec2-13-59-159-109.us-east-2.compute.amazonaws.com:81/api");
-
 angular.module("codeSnip")
-.constant("ApiUrl", "https://codesnippetsapi.azurewebsites.net/api");
+.constant("ApiUrl", "http://ec2-13-59-159-109.us-east-2.compute.amazonaws.com:81/api");
+
+// angular.module("codeSnip")
+// .constant("ApiUrl", "https://codesnippetsapi.azurewebsites.net/api");
